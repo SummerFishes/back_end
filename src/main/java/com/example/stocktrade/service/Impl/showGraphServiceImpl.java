18535fa8de9272e24d53f57864bddfe57f308f43
ctqandzmy <1144069701@qq.com>
2022-07-23 16:36:11 +0800

    后端第二次提交

--- src/main/java/com/example/stocktrade/service/Impl/showGraphServiceImpl.java
+++ src/main/java/com/example/stocktrade/service/Impl/showGraphServiceImpl.java
@@ -1,4 +1,11 @@
 package com.example.stocktrade.service.Impl;
 
+import com.example.stocktrade.dao.DateDataMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+import javax.annotation.Resource;
+
 public class showGraphServiceImpl {
+    @Resource
+    DateDataMapper dateDataMapper;
 }
